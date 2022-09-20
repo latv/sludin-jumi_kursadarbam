afc58ecab276ff668f67c8d69039919f95619e14
Janis <feldmanis45@gmail.com>
2022-09-20 20:30:00 +0300

    started adding deleting options to admin ID in comand line

--- api/commands.js
+++ api/commands.js
@@ -4,33 +4,51 @@ const config = require("./app/config/auth.config");
 require("dotenv").config();
 
 const options = yargs
-    .usage("Usage: -user_ID <name>")
-    .option("n", { alias: "userID", describe: "Your user ID to make you admin", type: "integer", demandOption: true })
+    .usage("Usage: -userToAdminID <name>")
+
+.option({ alias: "userToDeleteId", describe: "Your user ID to delete as admin", type: "integer" })
+    .argv;
+const optionsDelete = yargs
+    .usage("Usage: -userToDeleteAdminID <name>")
+    .option({ alias: "userToDeleteId", describe: "Your user ID to delete as admin", type: "integer" })
     .argv;
 
-
-
 const db = require("./app/models");
-const { user } = require("./app/models");
-// db.sequelize.sync();
+
+
 const User = db.user;
 const adminUserID = db.adminUserID;
-User.findOne({ where: { id: options.userID } }).then((result) => {
-    if (result === null) {
-        console.log("Nav atrasts lietotāja ID !");
-    } else {
-        adminUserID.findOne({ where: { userId: options.userID } }).then((res) => {
-                if (res === null) {
 
-                    adminUserID.create({ userId: options.userID });
-                    console.log("Ir piesķirta admina tiesības: ", options.userID, " lietotājam");
-                } else {
-                    console.log("Tev ir jau piešķirtas admina tiesības !")
+if (!(optionsDelete.userToDeleteId === true)) {
+    User.findOne({ where: { id: optionsDelete.userToDeleteId } }).then((result) => {
+        if (result === null) {
+            console.log("Nav atrasts lietotāja ID !");
+        } else {
+            adminUserID.findOne({ where: { userId: optionsDelete.userToDeleteId } }).then((res) => {
+                    if (res === null) {
+
+                        adminUserID.create({ userId: optionsDelete.userToDeleteId });
+                        console.log("Ir piesķirta admina tiesības: ", optionsDelete.userToDeleteId, " lietotājam");
+                    } else {
+                        console.log("Tev ir jau piešķirtas admina tiesības !")
+                    }
                 }
-            }
 
-        )
+            )
 
-    }
+        }
 
-});
+    });
+}
+
+if (!(options.userToDeleteId === true)) {
+
+    adminUserID.findOne({ where: { userId: options.userToDeleteId } }).then((result) => {
+        if (result === null) {
+            console.log("Nav atrasts lietotāja ID !");
+        } else {
+            adminUserID.destroy({ where: { userId: options.userToAdminID } });
+            console.log("Veiksmīgi izdzēsts lietotājs ar ID:", options.optionsDelete);
+        }
+    });
+}
